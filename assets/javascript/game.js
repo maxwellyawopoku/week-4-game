f9d502fd685dc6fcd822672cff9687e32f4596e5
maxwellyawopoku <maxwellyawopoku@gmail.com>
2017-08-15 19:45:29 -0400

    initial1 week-4-Game

---
+++ assets/javascript/game.js
@@ -0,0 +1,20 @@
+
+$(document).ready(function() {
+
+  
+
+
+
+
+    var targetNumber ;
+
+    targetNumber = (Math.floor(Math.random() * 100));
+
+
+    $("#number-to-guess").text(targetNumber);
+
+    console.log(targetNumber);
+
+  
+});
+
